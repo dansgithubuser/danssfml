f52e7165191e8fa10fd9ba79e70ec41ac934801a
Dan <dansonlinepresence@gmail.com>
2018-08-16 22:14:19 -0400

    bump minor version
    Really should be major because of breaking changes,
    but there's gonna be a lot of breaking changes.
    I should've started major version at 0.
    Oh well, 1 will be this package's 0.

--- wrapper/danssfmlpy/setup.py
+++ wrapper/danssfmlpy/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='danssfmlpy',
-	version='1.1.0',
+	version='1.2.0',
 	description='SFML wrapper for Python',
 	url='http://github.com/dansgithubuser/danssfml',
 	author='danspypiuser',
